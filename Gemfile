8062e1d7adc2648a9b5ef547a588da6cdb15b7bf
Bret Glad <gladfelty@gmail.com>
2012-02-07 13:57:35 -0800

    Remove defult Rails page

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.1'
 group :development, :test do 
 gem 'sqlite3', '1.3.5'
 gem 'rspec-rails', '2.8.1'
+gem 'guard-rspec', '0.5.5'
+gem 'guard', '1.0.0'
 end 
 
 
@@ -23,9 +25,15 @@ group :assets do
 end
 
 gem 'jquery-rails', '2.0.0'
-
+#Test gems on windows
 group :test do 
+  gem 'rspec-rails', '2.8.1'
+  gem 'rb-fchange','0.0.5'
+  gem 'rb-notifu', '0.0.4' 
+  gem 'win32console', '1.3.0'
   gem 'capybara', '1.1.2'
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '~>0.9.0.rc'
 end
 
 group :production do 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,23 +1,60 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
+
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit root_path
+      page.should have_selector('h1', text: 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit root_path
+      page.should have_selector('title',
+                        text: "Ruby on Rails Tutorial Sample App | Home")
     end
   end
+
   describe "Help page" do
-  it "should have the h1 'Help'" do
-    visit '/static_pages/help'
-    page.should have_selector('h1', :text => 'Help')
+
+    it "should have the h1 'Help'" do
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit help_path
+      page.should have_selector('title',
+                        text: "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
-  end
+
   describe "About page" do
+
     it "should have the h1 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit about_path
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end 
